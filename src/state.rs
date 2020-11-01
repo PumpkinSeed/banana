c17344028218a8047917d7ce2031eb873b3b4973
PumpkinSeed <qwer.kocka@gmail.com>
2020-11-01 17:53:05 +0000

    Start to work on balances

--- src/state.rs
+++ src/state.rs
@@ -1,9 +1,11 @@
+use cosmwasm_std::{Api, CanonicalAddr, HumanAddr, Coin, ReadonlyStorage, StdError, StdResult, Storage, Uint128};
+
+
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 //use serde_json::{Map, Value};
 
-use cosmwasm_std::{CanonicalAddr, Storage};
-use cosmwasm_storage::{singleton, singleton_read, ReadonlySingleton, Singleton};
+use cosmwasm_storage::{singleton, singleton_read, ReadonlySingleton, Singleton, PrefixedStorage, ReadonlyPrefixedStorage,};
 
 pub static CONFIG_KEY: &[u8] = b"config";
 
@@ -20,4 +22,18 @@ pub fn config<S: Storage>(storage: &mut S) -> Singleton<S, State> {
 
 pub fn config_read<S: Storage>(storage: &S) -> ReadonlySingleton<S, State> {
     singleton_read(storage, CONFIG_KEY)
+}
+
+// Balances
+
+pub struct ReadonlyBalances<'a, S: ReadonlyStorage> {
+    storage: ReadonlyPrefixedStorage<'a, S>,
+}
+
+impl<'a, S: ReadonlyStorage> ReadonlyBalances<'a, S> {
+    pub fn from_storage(storage: &'a S) -> Self {
+        Self {
+            storage: ReadonlyPrefixedStorage::new("", storage),
+        }
+    }
 }
