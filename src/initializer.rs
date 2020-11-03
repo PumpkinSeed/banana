6afc68564f180b45ed75ed69386c023ac55228a0
PumpkinSeed <qwer.kocka@gmail.com>
2020-11-03 09:40:56 +0000

    Add balances to the init

--- src/initializer.rs
+++ src/initializer.rs
@@ -1,16 +1,34 @@
-use cosmwasm_std::{Api, Env, Extern, InitResponse, Querier, StdResult, Storage};
+use cosmwasm_std::{Api, Env, Extern, InitResponse, Querier, StdError, StdResult, Storage};
 //use serde_json::{Map};
 use crate::msg::InitMsg;
-use crate::state::{config, State};
+use crate::state::{config, Balances, State};
 
 pub fn init<S: Storage, A: Api, Q: Querier>(
     deps: &mut Extern<S, A, Q>,
     env: Env,
     msg: InitMsg,
 ) -> StdResult<InitResponse> {
+    let mut total_supply: u128 = 0;
+    {
+        let mut balances = Balances::from_storage(&mut deps.storage);
+        let initial_balances = msg.initial_balances.unwrap_or_default();
+        for balance in initial_balances {
+            let balance_address = deps.api.canonical_address(&balance.address)?;
+            let amount = balance.amount.u128();
+            balances.set_account_balance(&balance_address, amount);
+            if let Some(new_total_supply) = total_supply.checked_add(amount) {
+                total_supply = new_total_supply;
+            } else {
+                return Err(StdError::generic_err(
+                    "The sum of all initial balances exceeds the maximum possible total supply",
+                ));
+            }
+        }
+    }
+
     let state = State {
         count: msg.count,
-    //    balances: Map::new(),
+        //    balances: Map::new(),
         owner: deps.api.canonical_address(&env.message.sender)?,
     };
 
@@ -22,14 +40,14 @@ pub fn init<S: Storage, A: Api, Q: Querier>(
 #[cfg(test)]
 mod tests {
     use super::*;
+    use cosmwasm_std::coins;
     use cosmwasm_std::testing::{mock_dependencies, mock_env};
-    use cosmwasm_std::{coins};
 
     #[test]
     fn proper_initialization() {
         let mut deps = mock_dependencies(20, &[]);
 
-        let msg = InitMsg { count: 17 };
+        let msg = InitMsg::default();
         let env = mock_env("creator", &coins(1000, "earth"));
 
         // we can just call .unwrap() to assert this was a success
--- src/msg.rs
+++ src/msg.rs
@@ -1,11 +1,26 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-use cosmwasm_std::{CanonicalAddr};
+use cosmwasm_std::{CanonicalAddr, HumanAddr, Uint128};
 
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InitialBalance {
+    pub address: HumanAddr,
+    pub amount: Uint128,
+}
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InitMsg {
     pub count: i32,
+    pub initial_balances: Option<Vec<InitialBalance>>,
+}
+
+impl InitMsg {
+    pub fn default() -> InitMsg {
+        InitMsg{
+            count: 0,
+            initial_balances: None,
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
